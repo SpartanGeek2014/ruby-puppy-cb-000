e97d1cc7fb7da2c6d35f13192391bd63380b3334
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-22 21:48:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,4 +2,20 @@
 
 class Dog
 
+  attr_accessor :name
+
+  @@all = dogs
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    puts @@all.map { |dog| dog.name  }
+  end
+
+  def self.clear_all
+    @@all.clear_all
+  end
 end
