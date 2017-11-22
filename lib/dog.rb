a0b1585acd3a8cc31f16e22775cbe5989ab1b6b5
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-22 21:50:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,16 @@
 class Dog
 
   attr_accessor :name
-
-  @@all = dogs
+  # creates a class variable and an empty array
+  @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
+    @@all << self #tells the program what to add to the empty array
   end
 
   def self.all
-    puts @@all.map { |dog| dog.name  }
+    puts @@all.map { |dog| dog.name  } # defines what ALL will be and where to find it
   end
 
   def self.clear_all
